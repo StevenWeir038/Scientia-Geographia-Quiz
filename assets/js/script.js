1c6848c8d761585c20e0df94b9c4ff6451d8596c
StevenWeir038 <weir_steven@btinternet.com>
2021-10-09 10:49:51 +0000

    Add quizLeave function with eventlistener

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const landingSection =  document.getElementById('landing');
 const newGameSection =  document.getElementById('new-game');
 const playQuizButton = document.getElementById('play-quiz-btn');
 const playerName = document.getElementById('player-name');
+const quizLeaveBtn = document.getElementById('quiz-leave-btn');
 
 function startNewGame() {
     landingSection.style.display = 'none';
@@ -11,3 +12,16 @@ function startNewGame() {
 }
 
 playQuizButton.addEventListener('click', startNewGame);
+
+
+function quizLeave() {
+    // clear input box
+    playerName.value = '';
+    console.log('playerName input box cleared');
+    // hide new game section, show landing section
+    landingSection.style.display = 'inline-flex';
+    newGameSection.style.display = 'none';
+    console.log('quizLeave function called');
+}
+
+quizLeaveBtn.addEventListener('click', quizLeave);
