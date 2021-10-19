2c355b3eff051e82b10a412732137b49b2bb4985
StevenWeir038 <weir_steven@btinternet.com>
2021-10-19 15:46:11 +0000

    Add more quiz questions for variety

--- assets/js/script.js
+++ assets/js/script.js
@@ -120,115 +120,181 @@ const quizQuestions = [{
     correctAns: "71%"
   },
   {
-  questionNum: '12',
-  questionText: "The Grand Canyon can be found in which US state?",
-  choices: [
-    "Hawaii",
-    "Oregon",
-    "Arizona",
-    "Alabama"
-  ],
-  correctAns: "Arizona"
-},
-{
-questionNum: '13',
-questionText: "Which two European countries does the Simplon Tunnel connect?",
-choices: [
-  "Scotland and Ireland",
-  "Denmark and Sweden",
-  "France and Italy",
-  "Switzerland and Italy"
-],
-correctAns: "Switzerland and Italy"
-},
-{
-questionNum: '14',
-questionText: "What is the longest river in Asia?",
-choices: [
-  "Yenesei",
-  "Mekong",
-  "Yangtze",
-  "Indus"
-],
-correctAns: "Yangtze"
-},
-{
-questionNum: '15',
-questionText: "Upon the U leaving the EU, how many states remain?",
-choices: [
-  "28",
-  "27",
-  "26",
-  "25"
-],
-correctAns: "27"
-},
-{
-questionNum: '16',
-questionText: "Which is the largest inland sea?",
-choices: [
-  "Caspian Sea",
-  "Black Sea",
-  "Mediterranean Sea",
-  "Dead Sea"
-],
-correctAns: "Caspain Sea"
-},
-{
-questionNum: '17',
-questionText: " Which is the smallest independent state in the world?",
-choices: [
-  "San Marino",
-  "Luxembourg",
-  "Andorra",
-  "The Vatican"
-],
-correctAns: "The Vatican"
-},
-{
-questionNum: '18',
-questionText: "Which range of mountains separates France from Spain?",
-choices: [
-  "Alps",
-  "Pyrenees",
-  "Ural",
-  "Mournes"
-],
-correctAns: "Pyrenees"
-},
-{
-questionNum: '19',
-questionText: "What is not GNSS system?",
-choices: [
-  "Galileo",
-  "Baidou",
-  "USNO",
-  "GLONASS"
-],
-correctAns: "USNO"
-},
-{
-questionNum: '20',
-questionText: "The Name Of Which Central American Country Means The Saviour In English?",
-choices: [
-  "Mexico",
-  "Honduras",
-  "El Salvador",
-  "Belize"
-],
-correctAns: "El Salvador"
-},
-{
-questionNum: '21',
-questionText: "Climate change is likely to create...",
-choices: [
-  "more extreme weather events",
-  "mass migration and food/water insecurity",
-  "disrupt the global economy",
-  "all of the above"
-],
-correctAns: "all of the above"
-}
+    questionNum: '12',
+    questionText: "The Grand Canyon can be found in which US state?",
+    choices: [
+      "Hawaii",
+      "Oregon",
+      "Arizona",
+      "Alabama"
+    ],
+    correctAns: "Arizona"
+  },
+  {
+    questionNum: '13',
+    questionText: "Which two European countries does the Simplon Tunnel connect?",
+    choices: [
+      "Scotland and Ireland",
+      "Denmark and Sweden",
+      "France and Italy",
+      "Switzerland and Italy"
+    ],
+    correctAns: "Switzerland and Italy"
+  },
+  {
+    questionNum: '14',
+    questionText: "What is the longest river in Asia?",
+    choices: [
+      "Yenesei",
+      "Mekong",
+      "Yangtze",
+      "Indus"
+    ],
+    correctAns: "Yangtze"
+  },
+  {
+    questionNum: '15',
+    questionText: "The European Union consists of how many states?",
+    choices: [
+      "28",
+      "27",
+      "26",
+      "25"
+    ],
+    correctAns: "27"
+  },
+  {
+    questionNum: '16',
+    questionText: "Which is the largest inland sea?",
+    choices: [
+      "Caspian Sea",
+      "Black Sea",
+      "Mediterranean Sea",
+      "Dead Sea"
+    ],
+    correctAns: "Caspain Sea"
+  },
+  {
+    questionNum: '17',
+    questionText: " Which is the smallest independent state in the world?",
+    choices: [
+      "San Marino",
+      "Luxembourg",
+      "Andorra",
+      "The Vatican"
+    ],
+    correctAns: "The Vatican"
+  },
+  {
+    questionNum: '18',
+    questionText: "Which range of mountains separates France from Spain?",
+    choices: [
+      "Alps",
+      "Pyrenees",
+      "Ural",
+      "Mournes"
+    ],
+    correctAns: "Pyrenees"
+  },
+  {
+    questionNum: '19',
+    questionText: "What is not GNSS system?",
+    choices: [
+      "Galileo",
+      "Baidou",
+      "USNO",
+      "GLONASS"
+    ],
+    correctAns: "USNO"
+  },
+  {
+    questionNum: '20',
+    questionText: "The Name Of Which Central American Country Means The Saviour In English?",
+    choices: [
+      "Mexico",
+      "Honduras",
+      "El Salvador",
+      "Belize"
+    ],
+    correctAns: "El Salvador"
+  },
+  {
+    questionNum: '21',
+    questionText: "Climate change is likely to create...",
+    choices: [
+      "more extreme weather events",
+      "mass migration and food/water insecurity",
+      "disrupt the global economy",
+      "all of the above"
+    ],
+    correctAns: "all of the above"
+  },
+  {
+    questionNum: '22',
+    questionText: "What is the world's most remote human settlement?",
+    choices: [
+      "Tristan da Cunha",
+      "Hakahau",
+      "Barrow Town",
+      "Nuuk"
+    ],
+    correctAns: "Tristan da Cunha"
+  },
+  {
+    questionNum: '23',
+    questionText: "What country doesn't share a border with North Korea?",
+    choices: [
+      "China",
+      "South Korea",
+      "Japan",
+      "Russia"
+    ],
+    correctAns: "Japan"
+  },
+  {
+    questionNum: '24',
+    questionText: "Which African nation does not have a sea border?",
+    choices: [
+      "Benin",
+      "Lesotho",
+      "Tanzania",
+      "Democratic Republic of Congo"
+    ],
+    correctAns: "Lesotho"
+  },
+  {
+    questionNum: '25',
+    questionText: "The Wilson cycle describes what process?",
+    choices: [
+      "Sea-level rise",
+      "Redistibution of atmospheric heat",
+      "Glacial retreat",
+      "Continental drift"
+    ],
+    correctAns: "Continental drift"
+  },
+  {
+    questionNum: '26',
+    questionText: "The Chicxulub crater is associated with what deep time, mass extinction event?",
+    choices: [
+      "Cretaceous-tertiary",
+      "Archaen/Proterozoic",
+      "Permian/triassic",
+      "Devonian"
+    ],
+    correctAns: "Cretaceous-tertiary"
+  },
+  {
+    questionNum: '27',
+    questionText: "What civilisation is often credited with producing the first world map?",
+    choices: [
+      "Celts",
+      "Greeks",
+      "Babylonians",
+      "Xia dynasty"
+    ],
+    correctAns: "Greeks"
+  }
 ];
 
 const quizLength = 10;
@@ -307,7 +373,7 @@ const counter = document.getElementById('counter');
 let timer;
 
 function startTimer() {
-  timeLeft = 30;
+  timeLeft = 300;
   timer = setInterval(function () {
     countdown(timeLeft);
   }, 1000);
@@ -517,12 +583,14 @@ function evaluateAnswer(targetID) {
   console.log('user answer is ' + userAnswer);
   console.log('correct answer is ' + correctAnswer);
 
-  //evaluate if user's answer is correct & add 1 point to total, if incorrect do not add 1 point to total
+  //evaluate if user's answer is correct & add 1 point to total, if incorrect do not add 1 point to total, add/update a userAns property to the quizQuestion array
   if (correctAnswer === userAnswer) {
     correctNum++;
     console.log('the user score moves to ' + correctNum);
+    console.log("set userAns property value in quizQueztions[currentQuestion] to 'correct'");
   } else {
     console.log('the user score remains at ' + correctNum)
+    console.log("set userAns property value in quizQueztions[currentQuestion] to 'incorrect'");
   }
 }
 
